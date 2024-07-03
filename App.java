300d8bea4f28e1ab49ccd0b8217e8e09fa194893
Vignesh Vishwanath <45537865+Vignesh2406@users.noreply.github.com>
2024-07-02 22:22:37 -0400

    First file

---
+++ App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        TwoFourTree tft = new TwoFourTree(); 
+        System.out.println("Hello, World!");
+    }
+}
